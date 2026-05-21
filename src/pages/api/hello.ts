8ae6e65b90863892e216f7d2f06d41866e25d26b
Cyrkill <cyril.sion@decathlon.com>
2026-05-21 15:56:54 +0200

    pokemon

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -1,13 +1,34 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
+import { useState } from "react";
 
-type Data = {
+// TODO: Définir le type pour ta liste de Pokémons
+type Pokemon = {
   name: string;
+  id: number;
 };
 
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Data>,
+  res: NextApiResponse,
 ) {
-  res.status(200).json({ name: "John Doe" });
+
+   const { search } = req.query;
+
+  try {
+
+    const response = await fetch("https://pokeapi.co/api/v2/pokemon/");
+    const data = await response.json();
+
+    let results = data.results;
+    if (search && typeof search === "string") {
+      results = results.filter((pokemon: Pokemon) =>
+        pokemon.name.toLowerCase().includes(search.toLowerCase())
+      );
+    }
+
+    res.status(200).json({ pokemons: results });
+  } catch (error) {
+    res.status(500).json({ error: "Erreur serveur" });
+  }
 }
